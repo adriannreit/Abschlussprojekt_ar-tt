5f1d117179f9d71e7250e9ad6c5e4650fcb84aad
TT-187 <tt7917@mci4me.at>
2026-07-07 09:40:39 +0200

    Kraft und Leistungsberechnung initiert(GPSAuswertung wurde als Superklasse verwendet, Luftwiederstand wurde berücksichtigt, p wert und cw wert wurden standartwerte verwendet.) in der gps datei ein bischen mit den tests rumgespielt

---
+++ kraft_Leistungsberechnung.py
@@ -0,0 +1,46 @@
+import numpy as np
+#import pandas as pd
+from gps_auswertung import GPSAuswertung
+
+class Kraftberechnung(GPSAuswertung):
+    def __init__(self, gps_data):
+        super().__init__(gps_data)
+        self.df = None
+
+    def luftwiderstandskraft(self, p = 1.225, cW = 0.8):
+        if self.df is None or "geschw._m/s" not in self.df.columns:
+            self.geschwindigkeit()
+
+        if self.df is None or "geschw._m/s" not in self.df.columns:
+            raise ValueError("Daten müssen mit geschwindigkeit() vorbereitet werden, bevor luftwiderstandskraft() aufgerufen wird.")
+
+        self.df["Luftwiderstandskraft"] = 0.5 * p * cW * (self.df["geschw._m/s"] ** 2)
+        return self.df
+    
+    def kraft(self, masse_kg):
+        if self.df is None or "beschleunigung" not in self.df.columns or "steigung" not in self.df.columns or "Luftwiderstandskraft" not in self.df.columns:
+            self.beschleunigung()
+            self.steigung()
+            self.luftwiderstandskraft()
+
+        if self.df is None or "beschleunigung" not in self.df.columns:
+            raise ValueError("Daten müssen mit beschleunigung(), steigung() und luftwiderstandskraft() vorbereitet werden, bevor kraft() aufgerufen wird.")
+
+        self.df["Kraft"] = masse_kg * self.df["beschleunigung"] + masse_kg * 9.81 * np.sin(self.df["steigung"]) + self.df["Luftwiderstandskraft"]
+        return self.df
+    
+    def leistung(self, masse_kg):
+        if self.df is None or "Kraft" not in self.df.columns or "geschw._m/s" not in self.df.columns:
+            self.kraft(masse_kg)  # Beispielmasse, kann angepasst werden
+
+        if self.df is None or "Kraft" not in self.df.columns or "geschw._m/s" not in self.df.columns:
+            raise ValueError("Daten müssen mit kraft() vorbereitet werden, bevor leistung() aufgerufen wird.")
+
+        self.df["Leistung"] = self.df["Kraft"] * self.df["geschw._m/s"]
+        return self.df
+
+if __name__ == "__main__":
+    Kb =  Kraftberechnung("final_project_input_data.csv")
+    df = Kb.kraft(masse_kg= 100)
+    df = Kb.leistung(masse_kg = 100)
+    print(df[["Kraft","geschw._m/s", "Leistung"]].head(10))
