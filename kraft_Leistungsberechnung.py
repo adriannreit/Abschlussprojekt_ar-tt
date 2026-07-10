b6892f4ed653c3d4f0aff3b2ef1022d8524ddeb5
TT-187 <tt7917@mci4me.at>
2026-07-10 09:59:57 +0200

    Drehmomentsberchenung und Motorstromsberechnung hinzugefügt (29 zoll rad Motorstromkonstante = 0.35 Nm/A)

--- kraft_Leistungsberechnung.py
+++ kraft_Leistungsberechnung.py
@@ -3,8 +3,9 @@ from gps_auswertung import GPSAuswertung
 from luftdruckberechnung import rho
 
 class Kraftberechnung(GPSAuswertung):
-    def __init__(self, gps_data):
+    def __init__(self, gps_data , masse_kg):
         super().__init__(gps_data)
+        self.masse_kg = masse_kg
         self.df = None
 
     def luftwiderstandskraft(self, cW = 0.8):
@@ -19,7 +20,7 @@ class Kraftberechnung(GPSAuswertung):
         self.df["Luftwiderstandskraft"] = 0.5 * p * cW * (self.df["geschw._m/s"] ** 2)
         return self.df
     
-    def kraft(self, masse_kg):
+    def kraft(self):
         if self.df is None or "beschleunigung" not in self.df.columns or "steigung" not in self.df.columns or "Luftwiderstandskraft" not in self.df.columns:
             self.beschleunigung()
             self.steigung()
@@ -28,21 +29,41 @@ class Kraftberechnung(GPSAuswertung):
         if self.df is None or "beschleunigung" not in self.df.columns:
             raise ValueError("Daten müssen mit beschleunigung(), steigung() und luftwiderstandskraft() vorbereitet werden, bevor kraft() aufgerufen wird.")
 
-        self.df["Kraft"] = masse_kg * self.df["beschleunigung"] + masse_kg * 9.81 * np.sin(self.df["steigung"]) + self.df["Luftwiderstandskraft"]
+        self.df["Kraft"] = self.masse_kg * self.df["beschleunigung"] + self.masse_kg * 9.81 * np.sin(self.df["steigung"]) + self.df["Luftwiderstandskraft"]
         return self.df
     
-    def leistung(self, masse_kg):
+    def leistung(self):
         if self.df is None or "Kraft" not in self.df.columns or "geschw._m/s" not in self.df.columns:
-            self.kraft(masse_kg)  # Beispielmasse, kann angepasst werden
+            self.kraft() 
 
         if self.df is None or "Kraft" not in self.df.columns or "geschw._m/s" not in self.df.columns:
             raise ValueError("Daten müssen mit kraft() vorbereitet werden, bevor leistung() aufgerufen wird.")
 
         self.df["Leistung"] = self.df["Kraft"] * self.df["geschw._m/s"]
         return self.df
+    
+    def drehmoment(self, rad = 0.368):
+        if self.df is None or "Kraft" not in self.df.columns:
+            self.kraft()
+
+        if self.df is None or "Kraft" not in self.df.columns:
+            raise ValueError("Daten müssen mit kraft() vorbereitet werden, bevor drehmoment() aufgerufen wird.")
+        self.df["Drehmoment"] = self.df["Kraft"] * rad
+        return self.df
+    
+    def motorstrom(self, Motorstromkonstante = 0.35):
+        if self.df is None or "Drehmoment" not in self.df.columns:
+            self.drehmoment()
+
+        if self.df is None or "Drehmoment" not in self.df.columns:
+            raise ValueError("Daten müssen mit drehmoment() vorbereitet werden, bevor motorstrom() aufgerufen wird.")
+        self.df["Motorstrom"] = self.df["Drehmoment"] / Motorstromkonstante
+        return self.df
 
 if __name__ == "__main__":
-    Kb =  Kraftberechnung("final_project_input_data.csv")
-    df = Kb.kraft(masse_kg= 100)
-    df = Kb.leistung(masse_kg = 100)
-    print(df[["Kraft","geschw._m/s", "Luftwiderstandskraft"]].head(10))
+    Kb =  Kraftberechnung("final_project_input_data.csv", masse_kg = 100)
+    df = Kb.kraft()
+    df = Kb.leistung()
+    df = Kb.drehmoment()
+    df = Kb.motorstrom()
+    print(df[["Kraft","geschw._m/s", "Luftwiderstandskraft", "Motorstrom"]].head(10))
